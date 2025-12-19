7895e5be7cc7eb7944c992bf47b67b476a3dc493
SlavaYZMA <slavasolen@gmail.com>
2025-12-19 12:41:35 +0100

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -50,7 +50,11 @@ const AboutModal = ({ isOpen, onClose }: Props) => {
   );
 
   return (
-    <NavModal isOpen={isOpen} onClose={onClose} title={t('nav.about')}>
+    <NavModal 
+      isOpen={isOpen} 
+      onClose={onClose} 
+      title={t('nav.about')}
+    >
       {language === 'ru' ? contentRu : contentEn}
     </NavModal>
   );
